f7139ecb2dc2b3072e414dff69dc97aeda846e53
leo carrocio <leocarrocio@MacBook-leo.local>
2020-11-10 07:46:26 -0300

    se conecta server con solicitud y esta se lo pasa al front

---
+++ src/client/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import Main from './components/Main';
+
+const app = <Main articulos={window.__data__}/> // se pasa por este medio los datos a mi Main
+
+//con hydrate monta el componente en el dom que ya existe
+ReactDom.hydrate(app,document.getElementById('root')); 
